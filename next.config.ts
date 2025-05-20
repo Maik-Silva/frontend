bcf3d6318eb150a4de20365db470f0f385ee47d3
Maik-Silva <maiknatanael20@gmail.com>
2025-05-20 20:14:52 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const nextConfig = {
               "default-src 'self' https://www.gstatic.com;",
               "script-src 'self' 'unsafe-inline' 'unsafe-eval';",
               "style-src 'self' 'unsafe-inline' https://www.gstatic.com;",
-              "connect-src 'self' http://localhost:5000 https://your-production-backend.com;",
+              "connect-src 'self' http://localhost:5000 https://backend-production-e77b.up.railway.app;",
               "img-src 'self' data:;"
             ].join(" "),
           },
@@ -20,4 +20,5 @@ const nextConfig = {
   },
   reactStrictMode: true,
 };
+
 module.exports = nextConfig;
